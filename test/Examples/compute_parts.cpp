7dc54f018b5efc917f30dbf04eaeb064a246ce9e
cheesema <cheesema@mpi-cbg.de>
2016-11-09 11:30:22 +0100

    added particle neighbour example

--- test/Examples/compute_parts.cpp
+++ test/Examples/compute_parts.cpp
@@ -84,13 +84,21 @@ int main(int argc, char **argv) {
     read_apr_pc_struct(pc_struct,file_name);
     
     //initialize variables required
-    uint64_t node_val; // node variable encoding neighbour and cell information
-    uint64_t y_coord; // y coordinate needs to be tracked and is not explicitly stored in the structure
+    uint64_t node_val_pc; // node variable encoding neighbour and cell information
+    uint64_t node_val_part; // node variable encoding part offset status information
     int x_; // iteration variables
     int z_; // iteration variables
     uint64_t j_; // index variable
     uint64_t curr_key = 0; // key used for accessing and particles and cells
-    PartCellNeigh<uint64_t> neigh_keys; // data structure for holding particle or cell neighbours
+    PartCellNeigh<uint64_t> neigh_part_keys; // data structure for holding particle or cell neighbours
+    PartCellNeigh<uint64_t> neigh_cell_keys;
+    //
+    // Extra variables required
+    //
+    
+    uint64_t status=0;
+    uint64_t part_offset=0;
+    uint64_t p;
     
     //
     //
@@ -104,14 +112,14 @@ int main(int argc, char **argv) {
     part_rep.timer.start_timer("Loop over parts and get neighbours");
     
     for(int i = pc_struct.pc_data.depth_min;i <= pc_struct.pc_data.depth_max;i++){
-        
+        //loop over the resolutions of the structure
         const unsigned int x_num_ = pc_struct.pc_data.x_num[i];
         const unsigned int z_num_ = pc_struct.pc_data.z_num[i];
         
         
-#pragma omp parallel for default(shared) private(z_,x_,j_,node_val,curr_key,neigh_keys) if(z_num_*x_num_ > 100)
+#pragma omp parallel for default(shared) private(p,z_,x_,j_,node_val_pc,node_val_part,curr_key,status,part_offset) firstprivate(neigh_part_keys,neigh_cell_keys) if(z_num_*x_num_ > 100)
         for(z_ = 0;z_ < z_num_;z_++){
-            
+            //both z and x are explicitly accessed in the structure
             curr_key = 0;
             
             pc_struct.pc_data.pc_key_set_z(curr_key,z_);
@@ -126,20 +134,32 @@ int main(int argc, char **argv) {
                 
                 const size_t j_num = pc_struct.pc_data.data[i][offset_pc_data].size();
                 
+                //the y direction loop however is sparse, and must be accessed accordinagly
                 for(j_ = 0;j_ < j_num;j_++){
                     
-                    node_val = pc_struct.pc_data.data[i][offset_pc_data][j_];
+                    //particle cell node value, used here as it is requried for getting the particle neighbours
+                    node_val_pc = pc_struct.pc_data.data[i][offset_pc_data][j_];
                     
-                    if (!(node_val&1)){
-                        //get the index gap node
+                    if (!(node_val_pc&1)){
+                        //Indicates this is a particle cell node
+                        node_val_part = pc_struct.part_data.access_data.data[i][offset_pc_data][j_];
                         
-                        pc_struct.pc_data.pc_key_set_j(curr_key,j_);
+                        pc_struct.part_data.access_data.pc_key_set_j(curr_key,j_);
                         
-                        pc_struct.pc_data.get_neighs_all(curr_key,node_val,neigh_keys);
+                        status = pc_struct.part_data.access_node_get_status(node_val_part);
+                        part_offset = pc_struct.part_data.access_node_get_part_offset(node_val_part);
                         
+                        //loop over the particles
+                        for(p = 0;p < pc_struct.part_data.get_num_parts(status);p++){
+                            //first set the particle index value in the particle_data array (stores the intensities)
+                            pc_struct.part_data.access_data.pc_key_set_index(curr_key,part_offset+p);
+                            //get all the neighbour particles in (+y,-y,+x,-x,+z,-z) ordering
+                            pc_struct.part_data.get_part_neighs_all(p,node_val_pc,curr_key,status,part_offset,neigh_cell_keys,neigh_part_keys,pc_struct.pc_data);
+                            
+                        }
                         
                     } else {
-                        
+                        // Inidicates this is not a particle cell node, and is a gap node
                     }
                     
                 }
@@ -149,10 +169,12 @@ int main(int argc, char **argv) {
         }
     }
     
-    std::cout << "Finished Neigh Cell test" << std::endl;
+    std::cout << "Finished Neigh Parts" << std::endl;
     
     
     part_rep.timer.stop_timer();
+    
+    uint64_t y_coord; // y coordinate needs to be tracked and is not explicitly stored in the structure
     
 }
 
