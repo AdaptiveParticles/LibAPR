f3f0e75a42ba0aed48f553ac2f13b5b2147587cd
cheesema <cheesema@mpi-cbg.de>
2016-11-09 13:51:03 +0100

    updated compute cells routine with some extra routines

--- test/Examples/compute_cells.cpp
+++ test/Examples/compute_cells.cpp
@@ -84,18 +84,18 @@ int main(int argc, char **argv) {
     
     //initialize
     uint64_t node_val;
-    uint64_t y_coord;
+    
     int x_;
     int z_;
     uint64_t j_;
     uint64_t curr_key = 0;
     PartCellNeigh<uint64_t> neigh_keys;
     
+    ///////////////////////////////////////////////
     //
+    //  Get all cell neighbours loop
     //
-    //  Get neighbour loop
-    //
-    //
+    ///////////////////////////////////////////////
     
     part_rep.timer.start_timer("Loop over cells and get neighbours");
     
@@ -110,6 +110,7 @@ int main(int argc, char **argv) {
             
             curr_key = 0;
             
+            //set the key values
             pc_struct.pc_data.pc_key_set_z(curr_key,z_);
             pc_struct.pc_data.pc_key_set_depth(curr_key,i);
             
@@ -120,22 +121,26 @@ int main(int argc, char **argv) {
                 
                 const size_t offset_pc_data = x_num_*z_ + x_;
                 
+                //number of nodes on the level
                 const size_t j_num = pc_struct.pc_data.data[i][offset_pc_data].size();
                 
                 for(j_ = 0;j_ < j_num;j_++){
                     
+                    //this value encodes the state and neighbour locations of the particle cell
                     node_val = pc_struct.pc_data.data[i][offset_pc_data][j_];
                     
                     if (!(node_val&1)){
-                        //get the index gap node
+                        //This node represents a particle cell
                         
+                        //set the key index
                         pc_struct.pc_data.pc_key_set_j(curr_key,j_);
                         
+                        //get all the neighbours
                         pc_struct.pc_data.get_neighs_all(curr_key,node_val,neigh_keys);
                         
                         
                     } else {
-                        
+                        //This is a gap node
                     }
                     
                 }
@@ -149,6 +154,117 @@ int main(int argc, char **argv) {
     
     
     part_rep.timer.stop_timer();
+    
+    
+    /////////////////////////////////////////////////////////////////////////
+    //
+    //  Get neighbour in +x direction and get status and compare with current cell status and get coordinates
+    //
+    /////////////////////////////////////////////////////////////////////////
+    
+    uint64_t y_coord; //keeps track of y coordinate
+    
+    
+    
+    part_rep.timer.start_timer("Loop over cells and compare +x neighbour status and get coordinates");
+    
+    for(int i = pc_struct.pc_data.depth_min;i <= pc_struct.pc_data.depth_max;i++){
+        
+        const unsigned int x_num_ = pc_struct.pc_data.x_num[i];
+        const unsigned int z_num_ = pc_struct.pc_data.z_num[i];
+        
+        
+#pragma omp parallel for default(shared) private(z_,x_,j_,node_val,curr_key,y_coord) firstprivate(neigh_keys) if(z_num_*x_num_ > 100)
+        for(z_ = 0;z_ < z_num_;z_++){
+            
+            curr_key = 0;
+            
+            //set the key values
+            pc_struct.pc_data.pc_key_set_z(curr_key,z_);
+            pc_struct.pc_data.pc_key_set_depth(curr_key,i);
+            
+            
+            for(x_ = 0;x_ < x_num_;x_++){
+                
+                pc_struct.pc_data.pc_key_set_x(curr_key,x_);
+                
+                const size_t offset_pc_data = x_num_*z_ + x_;
+                
+                //number of nodes on the level
+                const size_t j_num = pc_struct.pc_data.data[i][offset_pc_data].size();
+                
+                uint64_t status_current;
+                uint64_t x_current;
+                uint64_t y_current;
+                uint64_t z_current;
+                uint64_t depth_current;
+                
+                uint64_t x_neigh;
+                uint64_t y_neigh;
+                uint64_t z_neigh;
+                uint64_t depth_neigh;
+                uint64_t status_neigh;
+                
+                for(j_ = 0;j_ < j_num;j_++){
+                    
+                    //this value encodes the state and neighbour locations of the particle cell
+                    node_val = pc_struct.pc_data.data[i][offset_pc_data][j_];
+                    
+                    if (!(node_val&1)){
+                        y_coord++; //iterate y
+                        
+                        //This node represents a particle cell
+                        
+                        //set the key index
+                        pc_struct.pc_data.pc_key_set_j(curr_key,j_);
+                        
+                        //get some information about the current cell
+                        pc_struct.pc_data.get_coordinates_cell(y_coord,curr_key,x_current,z_current,y_current,depth_current,status_current);
+                        
+                        //get all the neighbours
+                        // (Neighbour directions are (+y,-y,+x,-x,+z,-z)
+                        uint64_t face = 2; // +x direction
+                    
+                        pc_struct.pc_data.get_neighs_face(curr_key,node_val,face,neigh_keys);
+                        
+                        //loop over the nieghbours
+                        for(int n = 0; n < neigh_keys.neigh_face[face].size();n++){
+                            // Check if the neighbour exisits (if neigh_cell_value = 0, the neighbour doesn't exist)
+                            uint64_t neigh_key = neigh_keys.neigh_face[face][n];
+                            
+                            if(neigh_key > 0){
+                                //get information about the nieghbour (need to provide face and neighbour number (n) and the current y coordinate)
+                                pc_struct.pc_data.get_neigh_coordinates_cell(neigh_keys,face,n,y_coord,y_neigh,x_neigh,z_neigh,depth_neigh);
+                                
+                                //Get the neighbour status, we need to access the cell
+                                uint64_t neigh_node = pc_struct.pc_data.get_val(neigh_key);
+                                //then we can get the status from this
+                                status_neigh = pc_struct.pc_data.get_status(neigh_node);
+                            }
+                            
+                            
+                        }
+                        
+                        
+                    } else {
+                        //This is a gap node
+                        
+                        //Gap nodes store the next and previous coodinate
+                        y_coord = (node_val & NEXT_COORD_MASK) >> NEXT_COORD_SHIFT;
+                        y_coord--; //set the y_coordinate to the value before the next coming up in the structure
+                    }
+                    
+                }
+                
+            }
+            
+        }
+    }
+    
+    
+    
+    part_rep.timer.stop_timer();
+    
     
 }
 
