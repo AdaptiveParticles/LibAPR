5c469a6236ec1f88a681d548047d89cc3fd07142
cheesema <cheesema@mpi-cbg.de>
2016-11-20 19:15:56 +0100

    added the base functions for the offset class that shoudl allow to do the filter loops, in y direction atleast

--- src/data_structures/Tree/PartCellOffset.hpp
+++ src/data_structures/Tree/PartCellOffset.hpp
@@ -22,12 +22,13 @@ public:
         depth = 0;
         x = 0;
         z = 0;
-        j = 0
+        j = 0;
         pc_offset = 0;
         y = 0;
         j_num = 0;
         x_num = 0;
         z_num = 0;
+        
     };
     
     void set_offsets(T offset_x_,T offset_z_,T offset_y_,T offset_depth_){
@@ -36,12 +37,13 @@ public:
         offset_z = offset_z_;
         offset_y = offset_y_;
         offset_depth = offset_depth_;
+        y_factor = pow(2.0,offset_depth_);
     }
     
     template<typename U>
     void set_new_depth(T depth_,PartCellStructure<U,T>& pc_struct){
         
-        depth = depth_ + offset_depth_;
+        depth = depth_ + offset_depth;
         x_num = pc_struct.x_num[depth];
         z_num = pc_struct.x_num[depth];
     
@@ -50,11 +52,12 @@ public:
     template<typename U>
     void set_new_xz(T x_,T z_,PartCellStructure<U,T>& pc_struct){
         
-        x = x_ + x_offset;
-        z = z_ + z_offset;
+        x = x_ + offset_x;
+        z = z_ + offset_z;
         
         pc_offset = x_num*z + x;
         j_num = pc_struct.pc_data.data[depth][pc_offset].size();
+        part_offset = 0;
         
         if(j_num > 1){
             y = (pc_struct.pc_data.data[depth][pc_offset][0] & NEXT_COORD_MASK) >> NEXT_COORD_SHIFT;
@@ -65,14 +68,16 @@ public:
     }
     
     template<typename U>
-    bool incriment_y(const T y_input,PartCellStructure<U,T>& pc_struct){
+    bool incriment_y(T y_input,PartCellStructure<U,T>& pc_struct){
         
+        //need to deal with offset and
+        y_input = floor((y_input+offset_y)*y_factor);
         
         //iterate forward
         while ((y < y_input) & (j < (j_num-1))){
             
             j++;
-            node_val = data[i][offset_pc_data_neigh][j_neigh];
+            node_val = pc_struct.pc_data.data[depth][pc_offset][j];
             
             if (node_val&1){
                 //get the index gap node
@@ -82,14 +87,32 @@ public:
             } else {
                 //normal node
                 y++;
+                part_offset += 1 + (((node_val & STATUS_MASK) >> STATUS_SHIFT) == SEED)*7;
             }
             
         }
         
-        return true //need to update this
+        if (y == y_input){
+            return true;
+        }else {
+            return false;
+        }
         
     }
     
+    template<typename U>
+    void incriment_y_and_update(T y_input,PartCellStructure<U,T>& pc_struct,std::vector<float>& temp_vec,std::vector<float>& temp_vec_depth){
+        
+        bool active;
+        //update the values
+        active = incriment_y(y_input,pc_struct);
+        
+        if(active == true){
+            temp_vec.back() = pc_struct.part_data.particle_data[depth][pc_offset][part_offset];
+            temp_vec_depth.back() = depth;
+        }
+        
+    }
     
 private:
     
@@ -102,12 +125,16 @@ private:
     T j_num;
     T x_num;
     T z_num;
+    T part_offset;
+    T node_val;
     
     //offsets
-    T x_offset;
-    T z_offset;
-    T y_offset;
-    T depth_offset
+    T offset_x;
+    T offset_z;
+    T offset_y;
+    T offset_depth;
+    
+    float y_factor;
 };
 
 #endif //PARTPLAY_PARTCELLOFFSET_HPP
