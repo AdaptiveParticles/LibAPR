bd8ac01b19ae3c798a66e0db8f0ee01efb2035ff
cheesema <cheesema@mpi-cbg.de>
2017-01-23 13:15:44 +0100

    adding analysis functions

---
+++ src/analysis/apr_analysis.h
@@ -0,0 +1,29 @@
+//
+// Created by cheesema on 23/01/17.
+//
+
+#ifndef PARTPLAY_ANALYZE_APR_H
+#define PARTPLAY_ANALYZE_APR_H
+
+
+template<typename S,typename U>
+void compare_reconstruction_to_original(Mesh_data<S>& org_img,P){
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //PARTPLAY_ANALYZE_APR_H
