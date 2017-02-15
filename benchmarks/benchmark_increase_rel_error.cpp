304c259e725125f953f538da200f21f3ec1743c6
Bevan Cheeseman <cheesema@mpi-cbg.de>
2017-02-15 17:42:10 +0100

    about to run the 100 benchmark

--- benchmarks/benchmark_increase_rel_error.cpp
+++ benchmarks/benchmark_increase_rel_error.cpp
@@ -94,11 +94,11 @@ int main(int argc, char **argv) {
     }
 
 
-    sig_vec = {1,3,8};
+    sig_vec = {3};
 
     //min mean
     float min_shift = 5;
-    float max_shift = 10000;
+    float max_shift = 1000;
     num_steps = 0;
 
     del = (max_shift - min_shift) / num_steps;
