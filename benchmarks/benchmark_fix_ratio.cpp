7e8392943ff8881df30f12e7de450a559536fc4d
cheesema <cheesema@mpi-cbg.de>
2017-02-16 08:43:27 +0100

    making ration via command line

--- benchmarks/benchmark_fix_ratio.cpp
+++ benchmarks/benchmark_fix_ratio.cpp
@@ -92,7 +92,7 @@ int main(int argc, char **argv) {
         image_size.push_back(i);
     }
 
-    float ratio = 20;
+    float ratio = options.delta;
 
     int N_par = (int)image_size.size(); // this many different parameter values to be run
 
