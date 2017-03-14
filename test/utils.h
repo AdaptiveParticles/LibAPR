573982ae1104fb8547aa2a76a890d9f39493aeeb
cheesema <cheesema@mpi-cbg.de>
2017-03-14 16:35:16 +0100

    added git ignore for natives

--- test/utils.h
+++ test/utils.h
@@ -3583,7 +3583,7 @@ bool utest_moore_neighbours(PartCellStructure<float,uint64_t>& pc_struct){
     //  Testing for full moore neighbourhood, it is seprated into face, edge, and corner.
     //
 
-
+    std::cout << "Should break here" << std::endl;
     ParticleDataNew<float, uint64_t> part_new;
     //flattens format to particle = cell, this is in the classic access/part paradigm
     part_new.initialize_from_structure(pc_struct);
