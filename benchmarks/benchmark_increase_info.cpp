8c7e30123365c3ab4c644b1f247b1818d26ee09f
cheesema <cheesema@mpi-cbg.de>
2017-06-07 10:09:01 +0200

    re-running benchmark for increase info

--- benchmarks/benchmark_increase_info.cpp
+++ benchmarks/benchmark_increase_info.cpp
@@ -72,7 +72,7 @@ int main(int argc, char **argv) {
         number_obj.push_back(i);
     }
 
-    number_obj = {40};
+    //number_obj = {40};
 
     //////////////////////////////////////////////////////////
     //
@@ -100,7 +100,7 @@ int main(int argc, char **argv) {
 
     mean_int = {1,10,30};
 
-    mean_int = {30};
+   // mean_int = {30};
 
     bs.int_scale_min = 1;
     bs.int_scale_max = 10;
