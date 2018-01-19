158a1f9e473224fee1513411ecbbda630ba056fe
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-01-19 16:12:59 +0100

    Read info from TIFF added

--- src/io/Tiff.hpp
+++ src/io/Tiff.hpp
@@ -1,16 +1,36 @@
-
 #ifndef TIFF_HPP
 #define TIFF_HPP
 
 #include <string>
+#include <tiffio.h>
 
 class Tiff {
 public:
-    void printTiffInfo(const std::string &aFileName);
+    Tiff(const std::string &aFileName) { open(aFileName); }
+    ~Tiff() { close(); }
+
+    void printInfo();
 
 //    void write_image_tiff(std::string& filename);
 //    void write_image_tiff_uint16(std::string& filename);
-//    void load_image_tiff(std::string file_name,int z_start = 0, int z_end = -1);
+    void getMesh(int aStartZ = 0, int aEndZ = -1);
+
+private:
+    enum class TiffType {TIFF_UINT8, TIFF_UINT16, TIFF_FLOAT, TIFF_INVALID};
+
+    bool open(const std::string &aFileName);
+    void close();
+
+    TiffType iType = TiffType::TIFF_INVALID;
+    TIFF* iFile = nullptr;
+    std::string iFileName = "";
+    uint32 iImgWidth = 0;
+    uint32 iImgHeight = 0;
+    uint32 iNumberOfDirectories = 0;
+    unsigned short iSamplesPerPixel = 0;
+    unsigned short iBitsPerSample = 0;
+    unsigned short iSampleFormat = 0;
+    unsigned short iPhotometric = 0;
 };
 
 #endif
--- test/TiffTest.cpp
+++ test/TiffTest.cpp
@@ -3,8 +3,24 @@
 
 namespace {
     TEST(TiffTest, PrintInfoTest) {
-        Tiff t;
-        t.printTiffInfo("/hahaha");
+        Tiff t1("/Users/gonciarz/Documents/MOSAIC/work/repo/APR_FILES/101x122x8bit.tif");
+        t1.printInfo();
+
+        Tiff t2("/Users/gonciarz/Documents/MOSAIC/work/repo/APR_FILES/102x121x16bit.tif");
+        t2.printInfo();
+
+        Tiff t3("/Users/gonciarz/Documents/MOSAIC/work/repo/APR_FILES/102x122x3xfloat.tif");
+        t3.printInfo();
+
+        Tiff t4("/Users/gonciarz/Documents/MOSAIC/work/repo/APR_FILES/big.tif");
+        t4.printInfo();
+
+        Tiff t5("/Users/gonciarz/Documents/MOSAIC/work/repo/APR_FILES/10x20x30xRGB.tif");
+        t5.printInfo();
+
+        Tiff t6("/Users/gonciarz/Documents/MOSAIC/work/repo/APR_FILES/10x20x30xRGB.tiff");
+        t6.printInfo();
+
     }
 }
 
