584dcc6c93498beecc89b9a00d010ced49c05b87
cheesema <cheesema@mpi-cbg.de>
2018-02-19 11:18:15 +0100

    added first attemp to adaptive min, still has some issues

--- src/data_structures/APR/APRTreeIterator.hpp
+++ src/data_structures/APR/APRTreeIterator.hpp
@@ -39,6 +39,35 @@ public:
 
     }
 
+    void set_particle_cell_no_search(APRIterator<ImageType>& current_iterator){
+        this->current_particle_cell.y = (uint16_t)(current_iterator.y());
+        this->current_particle_cell.x = (uint16_t)(current_iterator.x());
+        this->current_particle_cell.z = (uint16_t)(current_iterator.z());
+        this->current_particle_cell.level = (uint16_t)(current_iterator.level());
+
+    }
+
+    bool find_neighbours_same_level(const uint8_t& direction){
+
+        bool found = false;
+
+        this->level_delta = _LEVEL_SAME;
+        this->apr_access->get_neighbour_coordinate(this->current_particle_cell,this->neighbour_particle_cell,direction,this->level_delta,0);
+
+        if(this->check_neighbours_particle_cell_in_bounds()){
+            if(this->apr_access->find_particle_cell(this->neighbour_particle_cell,this->apr_access->get_local_iterator(this->local_iterators, this->level_delta, direction,0))){
+                //found the neighbour! :D
+                found=true;
+            }
+        };
+
+        if(!found){
+            this->level_delta=_NO_NEIGHBOUR;
+        }
+
+        return found;
+
+    }
 
 };
 
