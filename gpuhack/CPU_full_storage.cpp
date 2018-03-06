72bf353cce76a231c10f6cf282ff2c6d5ac8cd12
cheesema <cheesema@mpi-cbg.de>
2018-03-06 18:32:05 +0100

    added the new data structure, and example of looping over also, checking that it works correclty

--- gpuhack/CPU_full_storage.cpp
+++ gpuhack/CPU_full_storage.cpp
@@ -68,7 +68,6 @@ int main(int argc, char **argv) {
     ExtraParticleData<uint64_t> globalIndexRow;
 
 
-
     uint64_t particle_number;
     //Basic serial iteration over all particles
     for (particle_number = 0; particle_number < aprIt.total_number_particles(); ++particle_number) {
@@ -85,6 +84,110 @@ int main(int argc, char **argv) {
 
     }
 
+
+    ///////////////////////////
+    ///
+    /// Sparse Data for GPU
+    ///
+    ///////////////////////////
+
+    std::vector<std::vector<uint64_t>> level_zx_index_start;
+    std::vector<uint16_t> y_explicit;
+    std::vector<uint16_t> particle_values;
+
+    int x = 0;
+    int z = 0;
+
+    uint64_t zx_counter = 0;
+    std::vector<uint64_t> level_offset;
+
+    level_offset.resize(aprIt.level_max()+1,UINT64_MAX);
+
+
+    for (int level = aprIt.level_min(); level <= aprIt.level_max(); ++level) {
+        level_offset[level] = zx_counter;
+
+        for (z = 0; z < aprIt.spatial_index_x_max(level); ++z) {
+            for (x = 0; x < aprIt.spatial_index_x_max(level); ++x) {
+
+                zx_counter++;
+                if (aprIt.set_new_lzx(level, z, x) < UINT64_MAX) {
+                    level_zx_index_start.push_back({aprIt.global_index(), aprIt.particles_zx_end(level, z,
+                                                                                                 x)-1}); //This stores the begining and end global index for each level_xz_row
+                } else {
+                    level_zx_index_start.push_back({UINT64_MAX, 0}); //This stores the begining and end global index for each level_
+                }
+
+                for (aprIt.set_new_lzx(level, z, x);
+                     aprIt.global_index() < aprIt.particles_zx_end(level, z,
+                                                                   x); aprIt.set_iterator_to_particle_next_particle()) {
+                    y_explicit.push_back(aprIt.y());
+                    particle_values.push_back(apr.particles_intensities[aprIt]);
+
+                }
+            }
+
+        }
+    }
+
+
+    ////////////////////
+    ///
+    /// Example of doing our level,z,x access using the GPU data structure
+    ///
+    /////////////////////
+
+    std::vector<uint16_t> test_access_data;
+
+
+    for (int level = aprIt.level_min(); level <= aprIt.level_max(); ++level) {
+
+        const int x_num = aprIt.spatial_index_x_max(level);
+        const int z_num = aprIt.spatial_index_z_max(level);
+
+        for (z = 0; z < aprIt.spatial_index_x_max(level); ++z) {
+            for (x = 0; x < aprIt.spatial_index_x_max(level); ++x) {
+                if(level_offset[level]<UINT64_MAX) {
+                    uint64_t level_xz_offset = level_offset[level] + x_num * z + x;
+                    if (level_zx_index_start[level_xz_offset].size() > 0) {
+                        uint64_t particle_index_begin = level_zx_index_start[level_xz_offset][0];
+                        uint64_t particle_index_end = level_zx_index_start[level_xz_offset][1];
+
+                        for (uint64_t global_index = particle_index_begin;
+                             global_index <= particle_index_end; ++global_index) {
+                            uint16_t current_y_value = y_explicit[global_index];
+                            uint16_t current_particle_value = particle_values[global_index];
+
+                            test_access_data.push_back(current_particle_value);
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    //////////////////////////
+    ///
+    /// Now check the data
+    ///
+    ////////////////////////////
+
+    bool success = true;
+
+    for (int i = 0; i < test_access_data.size(); ++i) {
+        if(apr.particles_intensities.data[i]!=test_access_data[i]){
+            success = false;
+        }
+    }
+
+    if(success){
+        std::cout << "PASS" << std::endl;
+    } else {
+        std::cout << "FAIL" << std::endl;
+    }
 
 
 }
