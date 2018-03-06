e6f590bc254a8e85316e5db9b538469389da73cc
cheesema <cheesema@mpi-cbg.de>
2018-03-06 22:03:39 +0100

    Cpu_neighbour_access_tree is now passing the test

--- gpuhack/Cpu_neighbour_access_Tree.cpp
+++ gpuhack/Cpu_neighbour_access_Tree.cpp
@@ -44,12 +44,12 @@ char* get_command_option(char **begin, char **end, const std::string &option);
 
 
 template<typename T,typename ParticleDataType>
-void update_dense_array(const uint64_t level,const uint64_t z,APR<uint16_t>& apr,APRIterator<uint16_t>& apr_iterator, APRIterator<uint16_t>& treeIterator, ExtraParticleData<uint8_t> &tree_data,MeshData<T>& temp_vec,ExtraParticleData<ParticleDataType>& particleData){
+void update_dense_array(const uint64_t level,const uint64_t z,APR<uint16_t>& apr,APRIterator<uint16_t>& apr_iterator, APRIterator<uint16_t>& treeIterator, ExtraParticleData<float> &tree_data,MeshData<T>& temp_vec,ExtraParticleData<ParticleDataType>& particleData) {
 
     uint64_t x;
 
     const uint64_t x_num_m = temp_vec.x_num;
-    const uint64_t y_num_m =  temp_vec.y_num;
+    const uint64_t y_num_m = temp_vec.y_num;
 
 
     uint64_t parent_number;
@@ -63,24 +63,25 @@ void update_dense_array(const uint64_t level,const uint64_t z,APR<uint16_t>& apr
         //  This loop recreates particles at the current level, using a simple copy
         //
 
-        uint64_t mesh_offset = (x+1)*y_num_m + x_num_m*y_num_m*(z % 3);
+        uint64_t mesh_offset = (x + 1) * y_num_m + x_num_m * y_num_m * (z % 3);
 
         apr_iterator.set_new_lzx(level, z, x);
         for (unsigned long gap = 0;
              gap < apr_iterator.number_gaps(); apr_iterator.move_gap(gap)) {
 
-            uint64_t y_begin = apr_iterator.current_gap_y_begin()+1;
-            uint64_t y_end =apr_iterator.current_gap_y_end()+1;
-            uint64_t index =apr_iterator.current_gap_index();
+            uint64_t y_begin = apr_iterator.current_gap_y_begin() + 1;
+            uint64_t y_end = apr_iterator.current_gap_y_end() + 1;
+            uint64_t index = apr_iterator.current_gap_index();
 
-            std::copy(particleData.data.begin() + index,particleData.data.begin() + index + (y_end - y_begin) + 1,temp_vec.mesh.begin() + mesh_offset + y_begin );
+            std::copy(particleData.data.begin() + index, particleData.data.begin() + index + (y_end - y_begin) + 1,
+                      temp_vec.mesh.begin() + mesh_offset + y_begin);
 
 
         }
 
     }
 
-    if(level > apr_iterator.level_min()) {
+    if (level > apr_iterator.level_min()) {
         const int y_num = apr_iterator.spatial_index_y_max(level);
 
         //
@@ -97,9 +98,9 @@ void update_dense_array(const uint64_t level,const uint64_t z,APR<uint16_t>& apr
                                                                              x /
                                                                              2); apr_iterator.set_iterator_to_particle_next_particle()) {
 
-                int y_m = std::min(2 * apr_iterator.y() + 2,y_num);
+                int y_m = std::min(2 * apr_iterator.y() + 2, y_num);
 
-                temp_vec.at(2 * apr_iterator.y()+1, x + 1, z % 3) = particleData[apr_iterator];
+                temp_vec.at(2 * apr_iterator.y() + 1, x + 1, z % 3) = particleData[apr_iterator];
                 temp_vec.at(y_m, x + 1, z % 3) = particleData[apr_iterator];
 
 
@@ -109,114 +110,19 @@ void update_dense_array(const uint64_t level,const uint64_t z,APR<uint16_t>& apr
     }
 
     /******** start of using the tree iterator for downsampling ************/
-	
-    if( level < apr_iterator.level_max()){
-	for(x = 0; x < apr.spatial_index_x_max(level); ++x){
-		 for(parent_number = treeIterator.particles_level_begin(level); parent_number < treeIterator.particles_level_end(level); ++parent_number) {
-                    		treeIterator.set_iterator_to_particle_by_number(parent_number);
-		    		temp_vec.at(treeIterator.y() / 2 + 1, x / 2 + 1, z % 3)  = tree_data[treeIterator];
-                 }	
-	}
-    }
-		
-    /******** end of using the tree iterator for downsampling *************/
- /*   if(level < apr_iterator.level_max()) {	
 
-        //
-        //  This is an interpolating from higher resolution particles to lower, if it was full this would be the simply average
-        //  of the 8 children particle cells.
-        //
-        //  However, there are not always 8 children nodes, therefore we need to keep track of the number of children.
-        //
+    if (level < apr_iterator.level_max()) {
+        for (x = 0; x < apr.spatial_index_x_max(level); ++x) {
+            for (treeIterator.set_new_lzx(level, z , x );
+                 treeIterator.global_index() < treeIterator.particles_zx_end(level, z ,
+                                                                             x ); treeIterator.set_iterator_to_particle_next_particle()) {
 
-        const uint64_t z_num_us =  apr_iterator.spatial_index_z_max(level+1);
-        const uint64_t x_num_us =  apr_iterator.spatial_index_x_max(level+1);
-
-        int x_ds=0;
-
-#ifdef HAVE_OPENMP
-#pragma omp parallel for schedule(dynamic) private(x_ds) firstprivate(apr_iterator)
-#endif
-        for (x_ds = 0; x_ds < apr.spatial_index_x_max(level); ++x_ds) {
-
-            std::vector<uint8_t> curr_counter;
-            curr_counter.resize(y_num_m,0);
-
-            for (int i = 0; i < 2; ++i) {
-
-                int x = 2*x_ds + i;
-
-                if ((x) < x_num_us) {
-
-                    for (apr_iterator.set_new_lzx(level + 1, 2 * z, x);
-                         apr_iterator.global_index() < apr_iterator.particles_zx_end(level + 1, 2 * z,
-                                                                                     x); apr_iterator.set_iterator_to_particle_next_particle()) {
-
-                        temp_vec.at(apr_iterator.y() / 2 + 1, x / 2 + 1, z % 3) =
-                                (1.0f * curr_counter[apr_iterator.y() / 2] *
-                                 temp_vec.at(apr_iterator.y() / 2 + 1, x / 2 + 1, z % 3) +
-                                        particleData[apr_iterator]) /
-                                (1.0f * (curr_counter[apr_iterator.y() / 2]) + 1.0f);
-
-                        curr_counter[apr_iterator.y() / 2]++;
-                    }
-                    if ((2 * z + 1) < z_num_us) {
-                        for (apr_iterator.set_new_lzx(level + 1, 2 * z + 1, x);
-                             apr_iterator.global_index() < apr_iterator.particles_zx_end(level + 1, 2 * z + 1,
-                                                                                         x); apr_iterator.set_iterator_to_particle_next_particle()) {
-
-                            temp_vec.at(apr_iterator.y() / 2 + 1, x / 2 + 1, z % 3) =
-                                    (1.0f * curr_counter[apr_iterator.y() / 2] *
-                                     temp_vec.at(apr_iterator.y() / 2 + 1, x / 2 + 1, z % 3) +
-                                            particleData[apr_iterator]) /
-                                    (1.0f * curr_counter[apr_iterator.y() / 2] +
-                                     1.0f);
-                            curr_counter[apr_iterator.y() / 2]++;
-
-                        }
-                    }
-                }
+                temp_vec.at(treeIterator.y() +1, x +1, z % 3) = tree_data[treeIterator];
             }
-
         }
-*/
-
-//#ifdef HAVE_OPENMP
-//#pragma omp parallel for schedule(guided) private(x) firstprivate(apr_iterator)
-//#endif
-//        for (x = 0; x < apr.spatial_index_x_max(level+1); ++x) {
-//
-//            for (apr_iterator.set_new_lzx(level + 1, 2 * z, x);
-//                 apr_iterator.global_index() < apr_iterator.particles_zx_end(level + 1, 2 * z,
-//                                                                             x); apr_iterator.set_iterator_to_particle_next_particle()) {
-//
-//                    temp_vec.at(apr_iterator.y() / 2 + 1, x / 2 + 1, z % 3) +=
-//                            apr.particles_intensities[apr_iterator] * (1.0 / 8.0f);
-//
-//
-//            }
-//
-//        }
-//
-//        if((2*z + 1) < z_num_us){
-//#ifdef HAVE_OPENMP
-//#pragma omp parallel for schedule(guided) private(x) firstprivate(apr_iterator)
-//#endif
-//            for (x = 0; x < apr.spatial_index_x_max(level + 1); ++x) {
-//
-//                for (apr_iterator.set_new_lzx(level + 1, 2 * z + 1, x);
-//                     apr_iterator.global_index() < apr_iterator.particles_zx_end(level + 1, 2 * z + 1,
-//                                                                                 x); apr_iterator.set_iterator_to_particle_next_particle()) {
-//                    temp_vec.at(apr_iterator.y() / 2 + 1, x / 2 + 1, z % 3) += apr.particles_intensities[apr_iterator]*(1.0f/8.0f);
-//
-//                }
-//
-//            }
-//        }
     }
-
-
 }
+		
 
 
 
@@ -268,6 +174,8 @@ int main(int argc, char **argv) {
 
     APRTreeIterator<uint16_t> treeIterator(apr_tree);
 
+    APRTreeIterator<uint16_t> parentIterator(apr_tree);
+
     for (unsigned int level = apr_iterator.level_min(); level <= apr_iterator.level_max(); ++level) {
         for (particle_number = apr_iterator.particles_level_begin(level);
              particle_number < apr_iterator.particles_level_end(level); ++particle_number) {
@@ -283,11 +191,27 @@ int main(int argc, char **argv) {
         }
     }
 
+    for (unsigned int level = treeIterator.level_max(); level < treeIterator.level_min(); --level) {
+        for (particle_number = treeIterator.particles_level_begin(level);
+             particle_number < treeIterator.particles_level_end(level); ++particle_number) {
+
+
+            treeIterator.set_iterator_to_particle_by_number(particle_number);
+
+            parentIterator.set_iterator_to_parent(treeIterator);
+
+            tree_intensity[parentIterator]= (tree_intensity[parentIterator]*tree_counter[parentIterator]*1.0f + tree_intensity[treeIterator])/(1.0f*(tree_counter[parentIterator]+1.0f));
+
+            tree_counter[parentIterator]++;
+        }
+    }
+
+
   	/**** Filling the inside ********/
 	uint64_t parent_number;
-	APRTreeIterator<uint16_t> parentIterator(apr_tree);
+
 	ExtraParticleData<uint8_t> child_counter(apr_tree);
-	ExtraParticleData<uint8_t> tree_data(apr_tree);
+	ExtraParticleData<float> tree_data(apr_tree);
 
         for (particle_number = 0; particle_number < apr.total_number_particles(); ++particle_number) {
             //This step is required for all loops to set the iterator by the particle number
@@ -331,56 +255,7 @@ int main(int argc, char **argv) {
 
 
 
-    timer.start_timer("APR parallel iterator neighbour loop by level");
 
-    for (int i = 0; i < num_rep; ++i) {
-        for (unsigned int level = apr_iterator.level_min(); level <= apr_iterator.level_max(); ++level) {
-#ifdef HAVE_OPENMP
-#pragma omp parallel for schedule(static) private(particle_number) firstprivate(apr_iterator, neighbour_iterator)
-#endif
-            for (particle_number = apr_iterator.particles_level_begin(level);
-                 particle_number < apr_iterator.particles_level_end(level); ++particle_number) {
-
-                //needed step for any  loop (update to the next part)
-                apr_iterator.set_iterator_to_particle_by_number(particle_number);
-
-                float temp2 = 0;
-
-                //loop over all the neighbours and set the neighbour iterator to it
-                for (int direction = 0; direction < 6; ++direction) {
-                    apr_iterator.find_neighbours_in_direction(direction);
-                    // Neighbour Particle Cell Face definitions [+y,-y,+x,-x,+z,-z] =  [0,1,2,3,4,5]
-
-                    float temp = 0;
-                    float counter = 0;
-
-                    for (int index = 0; index < apr_iterator.number_neighbours_in_direction(direction); ++index) {
-                        if(apr_iterator.number_neighbours_in_direction(direction) ==1) {
-                            if (neighbour_iterator.set_neighbour_iterator(apr_iterator, direction, index)) {
-                                //neighbour_iterator works just like apr, and apr_parallel_iterator (you could also call neighbours)
-                                temp += apr.particles_intensities[neighbour_iterator];
-                                counter++;
-
-                            }
-                        }
-
-
-                    }
-                    if(counter > 0) {
-                        temp2 += temp / counter;
-                    }
-
-                }
-
-                part_sum[apr_iterator] = temp2/6.0f;
-
-                //part_sum_standard[apr_iterator] = apr.particles_intensities[apr_iterator];
-
-            }
-        }
-    }
-
-    timer.stop_timer();
 
 
 
@@ -463,13 +338,13 @@ int main(int argc, char **argv) {
             z = 0;
 
             //initial condition
-           update_dense_array(level, z, apr, apr_iterator, treeIterator, tree_data, temp_vec,apr.particles_intensities);
+           update_dense_array(level, z, apr, apr_iterator, treeIterator, tree_intensity, temp_vec,apr.particles_intensities);
 
             for (z = 0; z < apr.spatial_index_z_max(level); ++z) {
 
                 if (z < (z_num - 1)) {
                     //update the next z plane for the access
-                    update_dense_array(level, z + 1, apr, apr_iterator, treeIterator, tree_data, temp_vec,apr.particles_intensities);
+                    update_dense_array(level, z + 1, apr, apr_iterator, treeIterator, tree_intensity, temp_vec,apr.particles_intensities);
                 } else {
                     // need to set (z+1)%3 to zero, zero boundary condition
 
@@ -512,6 +387,7 @@ int main(int argc, char **argv) {
 
                         part_sum_dense[apr_iterator] = neigh_sum/6.0f;
                         //part_sum_dense[apr_iterator] = temp_vec.at(k, i, z%3);
+                        //part_sum_dense[apr_iterator] = apr.particles_intensities[apr_iterator];
                     }
                 }
 
@@ -526,17 +402,17 @@ int main(int argc, char **argv) {
 
     //check the result
 
-//    MeshData<uint16_t> check_mesh;
-//
-//    apr.interp_img(check_mesh,part_sum_dense);
-//
-//    std::string image_file_name = options.directory +  "check.tif";
-//    TiffUtils::saveMeshAsTiff(image_file_name, check_mesh);
-//
-//    apr.interp_img(check_mesh,part_sum_standard);
-//
-//    image_file_name = options.directory +  "check_standard.tif";
-//    TiffUtils::saveMeshAsTiff(image_file_name, check_mesh);
+    MeshData<uint16_t> check_mesh;
+
+    apr.interp_img(check_mesh,part_sum_dense);
+
+    std::string image_file_name = options.directory +  "check.tif";
+    TiffUtils::saveMeshAsTiff(image_file_name, check_mesh);
+
+    apr.interp_img(check_mesh,part_sum_standard);
+
+    image_file_name = options.directory +  "check_standard.tif";
+    TiffUtils::saveMeshAsTiff(image_file_name, check_mesh);
 
     bool success = true;
     uint64_t f_c=0;
@@ -567,86 +443,6 @@ int main(int argc, char **argv) {
         std::cout << "FAIL" << " " << f_c <<  std::endl;
     }
 
-
-    ExtraParticleData<float> part_sum_dense2(apr);
-
-    timer.start_timer("Dense neighbour access");
-
-    for (int j = 0; j < num_rep; ++j) {
-
-        for (int level = apr_iterator.level_min(); level <= apr_iterator.level_max(); ++level) {
-
-            unsigned int z = 0;
-            unsigned int x = 0;
-
-            const int y_num = apr_iterator.spatial_index_y_max(level);
-            const int x_num = apr_iterator.spatial_index_x_max(level);
-            const int z_num = apr_iterator.spatial_index_z_max(level);
-
-            MeshData<float> temp_vec;
-            temp_vec.init(apr_iterator.spatial_index_y_max(level) + 2, apr_iterator.spatial_index_x_max(level) + 2, 3,
-                          0); //padded boundaries
-
-            z = 0;
-
-            //initial condition
-            update_dense_array(level, z, apr, apr_iterator, treeIterator, tree_data,temp_vec,apr.particles_intensities);
-
-            for (z = 0; z < apr.spatial_index_z_max(level); ++z) {
-
-                if (z < (z_num - 1)) {
-                    //update the next z plane for the access
-                    update_dense_array(level, z + 1, apr, apr_iterator,treeIterator, tree_data, temp_vec,apr.particles_intensities);
-                } else {
-                    // need to set (z+1)%3 to zero, zero boundary condition
-
-                    uint64_t index = temp_vec.x_num * temp_vec.y_num * ((z + 1) % 3);
-
-#ifdef HAVE_OPENMP
-#pragma omp parallel for schedule(static) private(x)
-#endif
-                    for (x = 0; x < apr.spatial_index_x_max(level); ++x) {
-
-                        std::fill(temp_vec.mesh.begin() + index + (x + 1) * temp_vec.y_num ,
-                                  temp_vec.mesh.begin() + index + (x + 2) * temp_vec.y_num , 0);
-                    }
-                }
-
-
-#ifdef HAVE_OPENMP
-#pragma omp parallel for schedule(dynamic) private(x) firstprivate(apr_iterator)
-#endif
-                for (x = 0; x < apr.spatial_index_x_max(level); ++x) {
-                    for (apr_iterator.set_new_lzx(level, z, x);
-                         apr_iterator.global_index() < apr_iterator.particles_zx_end(level, z,
-                                                                                     x); apr_iterator.set_iterator_to_particle_next_particle()) {
-                        float neigh_sum = 0;
-                        float counter = 0;
-
-                        const int k = apr_iterator.y() + 1; // offset to allow for boundary padding
-                        const int i = x + 1;
-
-                        for (int l = -1; l < 2; ++l) {
-                            for (int q = -1; q < 2; ++q) {
-                                for (int w = -1; w < 2; ++w) {
-                                    neigh_sum += temp_vec.at(k+w, i+q, (z+3+w)%3);
-                                }
-                            }
-                        }
-
-
-                        part_sum_dense2[apr_iterator] = neigh_sum/27.0f;
-                        //part_sum_dense[apr_iterator] = temp_vec.at(k, i, z%3);
-                    }
-                }
-
-
-            }
-        }
-    }
-
-
-    timer.stop_timer();
 
 
 
