a490ab4f6c7ca4c5cd7ad61cf16813d2d75a9973
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-05-08 10:14:49 +0200

    Naive test for python wrapper added

---
+++ test/pythonTest.py
@@ -0,0 +1,12 @@
+# Find path to script - all test files are kept there
+import os
+myPath = os.path.dirname(os.path.abspath(__file__))
+print("Python script directory [" + myPath + "]")
+
+# Check if APR can be read and if gives correct dimensions
+import pyApr
+import numpy as np
+apr=pyApr.AprShort()
+apr.read(myPath + '/files/Apr/sphere_120/sphere_apr.h5')
+img=np.array(apr, copy = False)
+assert img.shape == (120, 120, 120)
