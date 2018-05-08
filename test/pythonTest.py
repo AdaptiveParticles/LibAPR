a4d3087566d8d19921c01571fe6f6d19a6cc3f85
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-05-08 10:56:00 +0200

    Added some debug printouts to investigate problems on travis

--- test/pythonTest.py
+++ test/pythonTest.py
@@ -3,6 +3,11 @@ import os
 myPath = os.path.dirname(os.path.abspath(__file__))
 print("Python script directory [" + myPath + "]")
 
+print("DIR: [" + os.getcwd() + "]")
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    print("F: [" + f + "]");
+
 # Check if APR can be read and if gives correct dimensions
 import pyApr
 import numpy as np
