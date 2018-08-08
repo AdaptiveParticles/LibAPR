3a70d5ed32b374743adf802444f67871881f40fc
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2018-08-08 16:18:31 +0200

    Build fix

---
+++ src/misc/CudaMemory.cu
@@ -0,0 +1,39 @@
+#include "CudaMemory.hpp"
+
+inline cudaError_t checkCuda(cudaError_t result) {
+#if defined(DEBUG) || defined(_DEBUG)
+    if (result != cudaSuccess) {
+        fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
+        assert(result == cudaSuccess);
+    }
+#endif
+    return result;
+}
+
+template <typename T>
+inline T* getPinnedMemory(size_t aNumOfBytes) {
+    T *memory = nullptr;
+    cudaError_t result = checkCuda(cudaMallocHost((void**)&memory, aNumOfBytes) );
+    std::cout << "Allocating pinned memory " << aNumOfBytes << " at " << (void*)memory << " result " << result << std::endl;
+    return memory;
+};
+
+template <typename T>
+inline void freePinnedMemory(T *aMemory) {
+    std::cout << "Freeing pinned memory " << (void*)aMemory << std::endl;
+    cudaFreeHost(aMemory);
+}
+
+template void freePinnedMemory(char*);
+template void freePinnedMemory(uint8_t*);
+template void freePinnedMemory(uint16_t*);
+template void freePinnedMemory(short*);
+template void freePinnedMemory(int*);
+template void freePinnedMemory(float*);
+
+template char* getPinnedMemory(size_t);
+template uint8_t* getPinnedMemory(size_t);
+template uint16_t* getPinnedMemory(size_t);
+template short* getPinnedMemory(size_t);
+template int* getPinnedMemory(size_t);
+template float* getPinnedMemory(size_t);
--- src/misc/CudaMemory.hpp
+++ src/misc/CudaMemory.hpp
@@ -9,29 +9,10 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-inline cudaError_t checkCuda(cudaError_t result) {
-#if defined(DEBUG) || defined(_DEBUG)
-    if (result != cudaSuccess) {
-        fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
-        assert(result == cudaSuccess);
-    }
-#endif
-    return result;
-}
+template <typename T>
+inline T* getPinnedMemory(size_t aNumOfBytes);
 
 template <typename T>
-inline T* getPinnedMemory(size_t aNumOfBytes) {
-    T *memory = nullptr;
-    cudaError_t result = checkCuda(cudaMallocHost((void**)&memory, aNumOfBytes) );
-    std::cout << "Allocating pinned memory " << aNumOfBytes << " at " << (void*)memory << " result " << result << std::endl;
-    return memory;
-};
-
-template <typename T>
-inline void freePinnedMemory(T *aMemory) {
-    std::cout << "Freeing pinned memory " << (void*)aMemory << std::endl;
-    cudaFreeHost(aMemory);
-}
-
+inline void freePinnedMemory(T *aMemory);
 
 #endif //LIBAPR_CUDAMEMORY_HPP
