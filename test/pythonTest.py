dfb9dafd0d2b5972fcc30845ab4bd6bb9e957216
joeljonsson <joeljo@student.chalmers.se>
2018-09-20 15:40:00 +0200

    added search for module to pythonTest

--- test/pythonTest.py
+++ test/pythonTest.py
@@ -2,15 +2,31 @@
 import os, sys
 myPath = os.path.dirname(os.path.abspath(__file__))
 
-# Add path to build APR pythond module
-sys.path.insert(0, "../cmake-build-release")
+# Add path to location of the python APR module (your build folder)
+#sys.path.insert(0, "../cmake-build-release")
 
 # Check if APR can be read and if gives correct dimensions
-import pyApr
+#import pyApr
 import numpy as np
+import fnmatch
 
 
 def main():
+    """
+    If you get ImportErrors, or if you have more than one build containing a file pyApr.so, you may want to
+    manually supply the path by changing the block below to
+                        sys.path.insert(0, '/path/to/dir/containing/module/')
+    """
+    # -------------------------------------------------- #
+    print("Searching for pyApr module...")
+    basePath = os.path.join(myPath, os.pardir)
+    for root, dirnames, filenames in os.walk(basePath):
+        if fnmatch.filter(filenames, 'pyApr.so'):
+            sys.path.insert(0, root)
+            print('pyApr module found in {}'.format(root))
+    # -------------------------------------------------- #
+
+    import pyApr
 
     successes = 0
 
@@ -34,25 +50,36 @@ def main():
 
     print("Testing read APR...")
     try:
+        apr = pyApr.AprShort()
         apr.read_apr( os.path.join(myPath, 'files/Apr/sphere_120/sphere_apr.h5') )
         successes += 1
         print("success!")
     except:
         print("**** failed ****")
 
+
     print("Testing write APR...")
     try:
         outpath = os.path.join(myPath, 'writeaprtest')
-        apr.write_apr(outpath)
+
+        if apr:
+            apr.write_apr(outpath)
+        else:
+            apr = pyApr.AprShort()
+            apr.write_apr(outpath)
+
         successes += 1
         print("success!")
 
         outpath = os.path.join(myPath, 'writeaprtest_apr.h5')
         if os.path.exists(outpath):
+            print('removing temporarily created APR file')
             os.remove(outpath)
-            #print('{} found'.format(outpath))
+        else:
+            print('could not find temporarily created APR file for removal')
     except:
         print("**** failed ****")
+
 
     print("Testing piecewise constant pixel image reconstruction...")
     try:
@@ -63,6 +90,7 @@ def main():
     except:
         print("**** failed ****")
 
+
     print("Testing smooth pixel image reconstruction...")
     try:
         img = np.array(apr.reconstruct_smooth(), copy=False)
@@ -72,8 +100,12 @@ def main():
     except:
         print("**** failed ****")
 
+
     print("Testing set APRParameters...")
     try:
+        if not pars:
+            pars = pyApr.APRParameters()
+
         pars.Ip_th = 0
         pars.sigma_th = 0
         pars.sigma_th_max = 0
