26e48e6958cafeecd04cbaf8d5c0c529ede69bee
joeljonsson <joeljo@student.chalmers.se>
2019-07-11 13:07:06 +0200

    basic cuda tests, migrated in old cuda code for tree fill

--- test/GPUAPR.hpp
+++ test/GPUAPR.hpp
@@ -5,8 +5,12 @@
 #ifndef LIBAPR_GPUAPRTEST_HPP
 #define LIBAPR_GPUAPRTEST_HPP
 
+#include "data_structures/APR/access/GPUAccess.hpp"
 
-bool run_simple_test();
+void compute_spatial_info_gpu(GPUAccessHelper& access, std::vector<uint64_t>& temp);
 
+void run_simple_test(std::vector<uint64_t>& temp, uint64_t size);
+
+void check_access_vectors(GPUAccessHelper& access, std::vector<uint64_t>& y_vec_out, std::vector<uint64_t>& xz_end_vec_out, std::vector<uint64_t>& level_xz_vec_out);
 
 #endif //LIBAPR_GPUAPRTEST_CUH_HPP
