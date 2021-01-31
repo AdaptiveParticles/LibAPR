3d047edaaa41162b8d5348ed3f8fb9d62a88d4f6
joeljonsson <jonsson@mpi-cbg.de>
2021-01-31 14:39:01 +0100

    change particle data type to uint16 for compatibility with other examples, minor cleanup

--- examples/Example_denoise.hpp
+++ examples/Example_denoise.hpp
@@ -54,59 +54,66 @@ bool denoise_example(cmdLineOptionsDenoise& options){
 
     timer.verbose_flag = true;
 
-    // APR datastructure
+    // APR data structure
     APR apr;
+    ParticleData<uint16_t>parts;
 
+    //read APR and particles from file
     timer.start_timer("read apr");
-    //read file
     APRFile aprFile;
     aprFile.open(file_name,"READ");
     aprFile.read_apr(apr);
-
-    ParticleData<float>parts;
     aprFile.read_particles(apr,parts); //default read, will take the first particles added to the file
-
     aprFile.close();
     timer.stop_timer();
 
-    APRStencils aprStencils;
-
-    auto it = apr.iterator();
-
-    aprStencils.dim = it.number_dimensions(); //get the dimension from the file.
-
-    PixelData<float> recon_pc;
-
+    // reconstruct noisy input image by piecewise constant interpolation
     timer.start_timer("pc interp");
-    //perform piece-wise constant interpolation
+    PixelData<uint16_t> recon_pc;
     APRReconstruction::interp_img(apr,recon_pc, parts);
     timer.stop_timer();
 
+    // save reconstructed input as TIFF
     std::string image_file_name = options.directory +  apr.name + "_org.tif";
     TiffUtils::saveMeshAsTiffUint16(image_file_name, recon_pc,false);
 
-    //load in an APR
+    /// Start of APR denoising
+    timer.start_timer("train APR denoise");
+
+    // init denoising stencils
+    APRStencils aprStencils;
+    auto it = apr.iterator();
+    aprStencils.dim = it.number_dimensions(); //get the dimension from the file.
+
+    // learn stencil weights
     APRDenoise aprDenoise;
-
     aprDenoise.train_denoise(apr,parts,aprStencils);
+    timer.stop_timer();
 
-    ParticleData<float> parts_denoised;
+
+    // apply denoising stencils
+    timer.start_timer("apply APR denoise");
+    ParticleData<uint16> parts_denoised;
     aprDenoise.apply_denoise(apr,parts,parts_denoised,aprStencils);
+    timer.stop_timer();
 
+    // reconstruct image from denoised particles by piecewise constant interpolation
     timer.start_timer("pc interp");
-    //perform piece-wise constant interpolation
     APRReconstruction::interp_img(apr,recon_pc, parts_denoised);
     timer.stop_timer();
 
+    // save denosied image as TIFF
     image_file_name = options.directory +  apr.name + "_denoised.tif";
     TiffUtils::saveMeshAsTiff(image_file_name, recon_pc,false);
 
+    // write APR and denoised particles to file
+    timer.start_timer("write denoised APR to file");
     file_name = options.directory + options.output;
-
     aprFile.open(file_name,"WRITE");
     aprFile.write_apr(apr);
     aprFile.write_particles("particles",parts_denoised);
     aprFile.close();
+    timer.stop_timer();
 
     return true;
 }
