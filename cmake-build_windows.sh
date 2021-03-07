3796b9c31d8f4493a50e32427916d7c9f1a166a2
cheesema <cheesema@mpi-cbg.de>
2021-03-07 17:41:48 +0000

    Add correct compilation, but this is for 2019 so should fail the tests

--- cmake-build_windows.sh
+++ cmake-build_windows.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-
+set -x
+mkdir build
+cd build
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
 ./bootstrap-vcpkg.bat
 ./vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
 cd ..
-mkdir build
-cd build
-cmake -DAPR_BUILD_STATIC_LIB=ON -DAPR_BUILD_SHARED_LIB=ON -DAPR_BUILD_EXAMPLES=ON -DAPR_TESTS=ON -DAPR_USE_CUDA=OFF -DAPR_PREFER_EXTERNAL_BLOSC=ON -DAPR_PREFER_EXTERNAL_GTEST=ON ..
-cmake --build .
 
-CTEST_OUTPUT_ON_FAILURE=1 ctest -V
+Cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows -T ClangCL -DAPR_BUILD_EXAMPLES=ON -DAPR_PREFER_EXTERNAL_BLOSC=ON -DAPR_PREFER_EXTERNAL_GTEST=ON -DAPR_BUILD_STATIC_LIB=ON -DAPR_BUILD_SHARED_LIB=OFF -DAPR_USE_OPENMP=ON -DAPR_TESTS=ON ..
+cmake --build . --config Release
+
+ctest -V
