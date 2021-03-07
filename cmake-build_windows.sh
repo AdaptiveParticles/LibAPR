c336558ad8611f4d17685ea349084c90312a1061
cheesema <cheesema@mpi-cbg.de>
2021-03-07 15:59:24 +0000

    Edit script

--- cmake-build_windows.sh
+++ cmake-build_windows.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 git clone https://github.com/microsoft/vcpkg
-.\vcpkg\bootstrap-vcpkg.bat
-.\vcpkg\vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
-
+cd vcpkg
+./bootstrap-vcpkg.bat
+./vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
+cd ..
 mkdir build
 cd build
 cmake -DAPR_BUILD_STATIC_LIB=ON -DAPR_BUILD_SHARED_LIB=ON -DAPR_BUILD_EXAMPLES=ON -DAPR_TESTS=ON -DAPR_USE_CUDA=OFF -DAPR_PREFER_EXTERNAL_BLOSC=ON -DAPR_PREFER_EXTERNAL_GTEST=ON ..
