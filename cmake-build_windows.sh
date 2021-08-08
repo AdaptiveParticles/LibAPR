a77121e95c121b223bafcea6d3e73c563c3c6416
cheesema <cheesema@mpi-cbg.de>
2021-08-08 15:57:41 +0100

    Add eigen3 to the environment set up

--- cmake-build_windows.sh
+++ cmake-build_windows.sh
@@ -5,7 +5,7 @@ cd build
 git clone https://github.com/microsoft/vcpkg
 cd vcpkg
 ./bootstrap-vcpkg.bat
-./vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows
+./vcpkg.exe install blosc:x64-windows gtest:x64-windows tiff:x64-windows hdf5:x64-windows szip:x64-windows eigen3:x64-windows
 cd ..
 
 cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE="vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET=x64-windows -T ClangCL -DAPR_BUILD_EXAMPLES=ON -DAPR_BUILD_STATIC_LIB=ON -DAPR_BUILD_SHARED_LIB=OFF -DAPR_TESTS=ON -DAPR_DENOISE=ON ..
